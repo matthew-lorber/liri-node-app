9852ef24c54a9dcea759b756cc07107c341fe9ba
Matt Lorber <matthew.lorber@outlook.com>
2019-10-20 12:24:35 -0400

    update to node_modules, heroku deployment

--- liri.js
+++ liri.js
@@ -1,8 +1,16 @@
+// Read and set environment variables
 require("dotenv").config();
 
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+
+   // prints `34e84d93de6a4650815e5420e0` to the console
+   console.log(process.env.SPOTIFY_ID) 
+
+   // prints `5162cd8b5cf940f48702df` to the console
+   console.log(process.env.SPOTIFY_SECRET)
+   // etc.
 
 // COMMANDS
 
